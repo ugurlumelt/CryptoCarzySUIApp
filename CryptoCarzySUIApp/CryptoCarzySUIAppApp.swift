cd0cac17d117845bbdee7046b4312e59aa98fcc4
Meltem Ugurlu <ugurlumeltem7@icloud.com>
2023-06-21 13:36:00 +0300

    Initial Commit

---
+++ CryptoCarzySUIApp/CryptoCarzySUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCarzySUIAppApp.swift
+//  CryptoCarzySUIApp
+//
+//  Created by Meltem Uğurlu on 21.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCarzySUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
